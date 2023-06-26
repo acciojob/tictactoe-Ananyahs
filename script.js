e7504110df6945c383654c4cf1a7d4d5f391123e
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2023-06-26 12:15:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let p1;
+let p2;
+
+const btnStart = document.getElementById("btnStart");
+btnStart.addEventListener("click",function(e){
+	e.preventDefault();
+	const player1 = document.getElementById("p1");
+	const player2 = document.getElementById("p2");
+	p1 = player1.value;
+	p2 = player2.value;
+	this.parentElement.remove();
+});
+const item1 = document.getElementById("item1");
+item1.addEventListener("click",function(){
+	item1.innerText = "x";
+})
